d096af89765e0d4fe0c66f7d9cb6c5966d467b81
Artur Dantas <arturdantasrodrigues@gmail.com>
2022-11-09 12:13:57 -0300

    Adicionando banner

--- pages/index.js
+++ pages/index.js
@@ -29,12 +29,6 @@ function HomePage() {
 export default HomePage
 
 const StyledHeader = styled.div`
-   img {
-      width: 80px;
-      height: 80px; 
-      border-radius: 50%;
-   }
-
    .user-info{
       display: flex;
       align-items: center;
@@ -42,12 +36,24 @@ const StyledHeader = styled.div`
       padding: 16px 32px;
       gap: 16px;
    }
+   .user-info img {
+      width: 80px;
+      height: 80px; 
+      border-radius: 50%;
+   }
+
+
+   .banner {
+      height: 250px;
+      width: 100%;
+      object-fit: none;
+   }
 `
 
 function Header() {
    return (
       <StyledHeader>
-         <img src="" />
+         <img className="banner" src={config.banner} />
 
          <section className="user-info">
             <img src={`https://github.com/${config.github}.png`} />
